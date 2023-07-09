3d075c5f626742069ad3eb3e7a0f2a3ff05a5df7
ed <ed@nous.co>
2023-07-09 21:25:05 +0100

    tweaks to guessinput validation using pattern and title

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -7,14 +7,15 @@ function GuessInput() {
       className="guess-input-wrapper"
       onSubmit={(event) => {
         event.preventDefault();
-        console.log(guess);
+        console.log({ guess });
         setGuess("");
       }}
     >
       <label htmlFor="guess-input">Enter guess:</label>
       <input
         required
-        pattern="\w{5,5}"
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
         value={guess}
         onChange={(event) => {
           setGuess(event.target.value.toUpperCase());
